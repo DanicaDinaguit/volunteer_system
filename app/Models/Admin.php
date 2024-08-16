51589310f98a61682d8d4014e0960f08b64c4e12
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-08-16 21:50:52 +0800

    added primary key to the Admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -10,6 +10,9 @@ class Admin extends Authenticatable
     use HasFactory;
 
     protected $table = 'tbladmin';
+    protected $primaryKey = 'adminID'; // Specify your primary key column name
+    public $incrementing = true; // Set to true if primary key is auto-incrementing
+    protected $keyType = 'int'; // Use 'string' if the primary key is a UUID
     protected $fillable = [
         'first_name',
         'middle_name',
