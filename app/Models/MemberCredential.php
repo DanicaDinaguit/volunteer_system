b7391feaf47665e02443b8b1edbdc877bf991bf1
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-08-16 21:49:29 +0800

    added primary key to the member credential model

--- app/Models/MemberCredential.php
+++ app/Models/MemberCredential.php
@@ -11,6 +11,9 @@ class MemberCredential extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $table = 'tblmembercredentials';
+    protected $primaryKey = 'memberApplicationID'; // Specify your primary key column name
+    public $incrementing = true; // Set to true if primary key is auto-incrementing
+    protected $keyType = 'int'; // Use 'string' if the primary key is a UUID
 
     protected $fillable = [
         'positionID',
