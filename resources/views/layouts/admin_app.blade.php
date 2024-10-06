752cafbfebf230a52e09dc1138a7f212b1a4b2f7
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-10-06 14:20:05 +0800

    added the notification nav, deleted the certificate nav, added csrf token and bootstrap link

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin App')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
 </head>
 <body>
@@ -28,9 +33,9 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ route('admin.createEvent')}}">
+                        <a href="{{ route('admin.event')}}">
                             <img src="{{ asset('images/gallery.png') }}" alt="Services Icon">
-                            <span>Create Event</span>
+                            <span>Event</span>
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('admin.viewApplication') }}">
@@ -51,9 +56,9 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ route('admin.createCertification')}}">
-                            <img src="{{ asset('images/gallery.png') }}" alt="Services Icon">
-                            <span>Certificate</span>
+                        <a href="{{ route('admin.notification') }}">
+                            <img src="{{ asset('images/about.png') }}" alt="Contact Icon">
+                            <span>Notification</span>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -75,5 +80,11 @@
         @include('layouts.footer')
     </div>
     <script src="{{ asset('js/index.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
