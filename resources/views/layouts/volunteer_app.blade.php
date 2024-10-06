c70bc98218c8779ecf857e155b961f2d28399bee
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-10-06 14:15:08 +0800

    added the csrf token and bootstrap links in volunteer layout

--- resources/views/layouts/volunteer_app.blade.php
+++ resources/views/layouts/volunteer_app.blade.php
@@ -2,11 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Volunteer App')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap">
-
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script></div>
 </head>
 <body>
     
@@ -28,7 +36,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="gallery.html">
+                    <a href="{{ route('volunteer.messages') }}">
                         <img src="{{ asset('images/gallery.png') }}" alt="Services Icon">
                         <span>Messages</span>
                     </a>
@@ -70,6 +78,8 @@
     @include('layouts.footer')
     
     <script src="{{ asset('js/index.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
 </html>
 
