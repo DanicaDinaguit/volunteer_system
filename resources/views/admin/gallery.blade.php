5b217d6cf785e5ef7cfb21f42ed21493750c58cc
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-11-12 14:13:08 +0800

    added some changes to admin gallery but still not final

--- resources/views/admin/gallery.blade.php
+++ resources/views/admin/gallery.blade.php
@@ -3,20 +3,23 @@
 @section('title', 'Admin Gallery of Events')
     
 @section('content')
-    <div id="gallery-bg" style="margin-bottom: 110px;">
-        <div class="gallery" style="margin-top: 130px; margin-left: 100px; margin-right: 100px;">
-            <h1>Gallery of Events</h1>
-            <div class="search-bar">
-                <img src="Images/search-icon.png" class="search-icon" alt="Search Icon">
-                <input type="text" class="search-input" placeholder="Search Year">
-            </div>
-            <div id="gallery-results">
-                <div>
-                    <img src="" alt="Calendar Select Date Icon">
-                    <p>Select Date</p>
+<div id="gallery-bg" class="container-fluid py-5" style="">
+        <div class="gallery container">
+            <h1 class="text-center fw-bold mb-4">Gallery of Events</h1>
+            
+            <!-- Search Bar -->
+            <div class="d-flex justify-content-center mb-4">
+                <div class="search-bar d-flex align-items-center" style="position: relative; max-width: 400px; width: 100%;">
+                    <!-- <img src="Images/search-icon.png" class="search-icon" alt="Search Icon" style="position: absolute; left: 10px; width: 20px;"> -->
+                    <input type="text" id="searchYear" class="form-control ps-5" placeholder="Search by Year or Date" style="border-radius: 30px;">
                 </div>
-                <div id="gallery-images">
-
+            </div>
+            
+            <!-- Gallery Images -->
+            <div id="gallery-results">
+                <div id="gallery-images" class="row g-4 justify-content-center">
+                    <!-- Sample Gallery Item -->
+                    <!-- Use a loop to populate images dynamically from the Facebook API -->
                 </div>
             </div>
         </div>
