75874621adce131bc45dfb9ba842e2740cbed70b
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-11-12 14:01:25 +0800

    used the include for footer for volunteer signin

--- resources/views/volunteer/signIn.blade.php
+++ resources/views/volunteer/signIn.blade.php
@@ -12,7 +12,7 @@
     <div id="signinDiv">
         <div>
             <div style="display: flex; align-items: center;">
-                <img src="{{ asset('images/LOGO.png') }}" alt="Logo" style="margin-left: 15px; margin-top: 10px;">
+                <img src="{{ asset('images/LOGO1.png') }}" alt="Logo" style="margin-left: 15px; margin-top: 10px; width: 64px; height: 64px;">
                 <h1 class="signinWelcome" style="text-align: center;">Welcome Back!</h1>
             </div>
 
@@ -40,22 +40,6 @@
             <img src="{{ asset('images/bannerImg.png') }}" class="bannerImg" alt="Banner Image">
         </div>
     </div>
-    <footer id="footer">
-        <div id="socMedIcons">
-            <img src="{{ asset('images/Vector.png') }}" alt="Facebook">
-            <img src="{{ asset('images/Instagram.png') }}" alt="Instagram">
-            <img src="{{ asset('images/Email.png') }}" alt="Email">
-        </div>
-        <div class="footerTextContainer">
-            <p class="footerText">Join our mission. Volunteer, Help, 
-                Advocate. Get Started Today.</p>
-            <a class="footerText" href="">Privacy Notice</a>
-            <a class="footerText" href="">Terms of Use</a>
-            <a class="footerText" href="">Help Center</a>
-            <a class="footerText" href="">Contact Us</a>
-        </div>
-
-        © 2024 DumaVolunteer Hub. All rights reserved.
-    </footer>
+    @include('layouts.footer')
 </body>
 </html>
