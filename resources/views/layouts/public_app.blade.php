5db0bdb6b0988497ad41a6611b1a4db27d09c999
ibgahilomo <ibgahilomo.student@asiancollege.edu.ph>
2024-11-17 17:02:04 +0800

    no more blank pages between slides in automatic carousel

--- resources/views/layouts/public_app.blade.php
+++ resources/views/layouts/public_app.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <!-- AJAX CSS -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
         @yield('styles')
     </head>
@@ -19,7 +19,7 @@
             <nav class="navbar navbar-light navbar-expand-lg bg-body-tertiary" data-bs-theme="light">
                 <div class="container">
                     <!-- Logo -->
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand ps-5" href="#">
                         <img src="{{ asset('images/LOGO1.png') }}" class="logo" alt="Logo" width="55" height="55">
                     </a>
                     <!-- Toggle Button -->
@@ -55,7 +55,7 @@
                         <!-- Sidebar Body -->
                         <div class="offcanvas-body flex-column flex-lg-row p-lg-1">
                             <ul 
-                                class="navbar-nav justify-content-end fs-5 flex-grow-1 pe-3"
+                                class="navbar-nav justify-content-end fs-5 flex-grow-1 pe-5"
                             >
                                 <li class="nav-item mx-3">
                                     <a class="nav-link" aria-current="page" href="{{ route('index') }}">Home</a>
@@ -83,14 +83,16 @@
         <br><br><br>
         <!-- footer -->
         @include('layouts.footer')
+        <!-- Custom JS -->
         <script src="{{ asset('js/index.js') }}"></script>
+        <!-- JQuery JS -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <!-- AJAX JS -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-        <!-- Bootstrap JS -->
+        <!-- Popper JS -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+        <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
         <!-- FullCalendar -->
         <script src="https://cdn.jsdelivr.net/combine/npm/fullcalendar@6.1.15,npm/fullcalendar@6.1.15/index.global.min.js"></script>
         @yield('scripts')
