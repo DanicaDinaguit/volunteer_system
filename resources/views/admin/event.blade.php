489aa0bbd2a87035a6734e81d803044d79b384dc
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-11-20 15:44:44 +0800

    deleted a line not used

--- resources/views/admin/event.blade.php
+++ resources/views/admin/event.blade.php
@@ -173,8 +173,6 @@
             <div class="modal-header">
                 <h5 class="modal-title" id="createEventModalLabel">Create New Event</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-
-                <button></button>
             </div>
             <div class="modal-body">
                 <!-- Create Event Form -->
