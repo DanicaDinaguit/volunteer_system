2e797f13b824afb1babb0813902e413cd9b6618e
DanicaDinaguit <dinaguit.danica@gmail.com>
2024-11-25 09:43:58 +0800

    added the improved navbar according to collaborator two's changes

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin App')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    @yield('styles')
+    @yield('styles') 
     
     <!-- Favicon for different browsers -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
@@ -26,8 +26,7 @@
     <!-- Include the Html5-qrcode library -->
     <script src="{{ asset('js/html5-qrcode.min.js')}}"></script>
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
@@ -35,60 +34,85 @@
     <!-- <link href="{{ asset('css/fullcalendar.print.min.css') }}" rel="stylesheet" media="print"> -->
 </head>
 <body>
-    <div>
-        <div id="navdiv">
-            <!-- <div id="pre-navdiv"></div> -->
-            <nav>
-                <img src="{{ asset('images/LOGO1.png') }}" class="logo" alt="Logo">
-                <ul class="nav-items">
-                    <li class="nav-item">
-                        <a href="{{ route('admin.Home') }}">
-                            <span class="nav-text">Home</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.calendar') }}">
-                            <span class="nav-text">Calendar</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.event')}}">
-                            <span>Event</span>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.viewApplication') }}">
-                            <span class="nav-text">Applications</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.messages')}}">
-                            <span class="nav-text">Messages</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.gallery')}}">
-                            <span class="nav-text">Gallery</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('admin.notification') }}">
-                            <span class="nav-text">Notification</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#">
-                            <img src="{{ asset('images/aboutus.png') }}" alt="About Us Icon">
-                    </li>
-                    <li class="nav-item">
+    <!-- Navigation bar for logo and for Home Page general audience view -->
+    <nav class="navbar navbar-light navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="light">
+        <div class="container">
+            <!-- Logo -->
+            <a class="navbar-brand ps-md-5" href="{{ route('index') }}">
+                <img src="{{ asset('images/LOGO1.png') }}" class="logo" alt="Logo" width="55" height="55">
+            </a>
+            <!-- Toggle Button -->
+            <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="offcanvas" 
+                data-bs-target="#offcanvasNavbar" 
+                aria-controls="offcanvasNavbar" 
+                >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Sidebar -->
+            <div
+                class="sidebar offcanvas offcanvas-start"
+                tabindex="-1"
+                id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel"
+            >
+                <!-- Sidebar Header -->
+                <div class="offcanvas-header border-bottom">
+                    <!-- Sidebar Logo -->
+                    <a class="navbar-brand" href="{{ route('admin.Home') }}">
+                    <img src="{{ asset('images/LOGO1.png') }}" class="logo" alt="Logo" width="55" height="55">
+                    </a>
+                    <button
+                        type="button"
+                        class="btn-close shadow-none"
+                        data-bs-dismiss="offcanvas"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <!-- Sidebar Body -->
+                <div class="offcanvas-body flex-column flex-lg-row p-lg-1">
+                    <ul 
+                        class="navbar-nav justify-content-end fs-5 flex-grow-1 pe-md-5"
+                    >
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" aria-current="page" href="{{ route('admin.Home') }}">Home</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="{{ route('admin.calendar') }}">Calendar</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="{{ route('admin.event') }}">Event</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="{{ route('admin.viewApplication') }}">Application</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="{{ route('admin.messages') }}">Messages</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="{{ route('admin.gallery') }}">Gallery</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="{{ route('admin.notification') }}">Notification</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="#">
+                                <img src="{{ asset('images/aboutus.png') }}" class="logo" alt="Logo" width="20" height="20">
+                            </a>
+                        </li>
+                        <li class="nav-item">
                         <a href="{{ route('admin.profile') }}">
                             <img src="{{ asset('images/profile.png') }}" alt="Profile Icon">
                         </a>
                     </li>
-                </ul>  
-            </nav>
+                    </ul>
+                </div>
+            </div>  
         </div>
-        <br>
-
+    </nav>
+    <div>
         <main>
             @yield('content')
         </main>
